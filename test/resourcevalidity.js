924757ec0bc29be2aa4ba192f260c18ed2b06697
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-02-11 10:50:45 +1100

    Made a dumb mistake and inadvertantly invented a new method on the string prototype. Fixed, and a test has been added to ensure correct behaviour.

--- test/resourcevalidity.js
+++ test/resourcevalidity.js
@@ -6,7 +6,32 @@ var chai = require("chai");
 
 describe("Resource validity checker",function() {
 	
-	
+	it("should be able to determine whether a domain is a subdomain of another",
+		function() {
+		
+		var crawler = new (require("../"))("example.com",3000);
+		
+		// Enable scanning subdomains, important for this test
+		crawler.scanSubdomains = true;
+		
+		// The domain itself isn't a subdomain per-se, but should be allowed
+		crawler.domainValid("example.com").should.equal(true);
+		
+		// WWW is a subdomain
+		crawler.domainValid("www.example.com").should.equal(true);
+		
+		// More complex examples
+		crawler.domainValid("testing.example.com").should.equal(true);
+		
+		// Multiple levels
+		crawler.domainValid("system.cache.example.com").should.equal(true);
+		
+		// These aren't valid...
+		crawler.domainValid("com.example").should.equal(false);
+		crawler.domainValid("example.com.au").should.equal(false);
+		crawler.domainValid("example.us").should.equal(false);
+		
+	});
 	
 	
 });
