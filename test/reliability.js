25af355a54f58f78ef64fbb4f787b706440d4732
Christopher Giffard <christopher.giffard@cgiffard.com>
2014-04-21 17:58:57 +1000

    Queue: Corrected queue defrosting.
    * Fixes issue where caches and indexes associated with the queue
      were not updated when defrosting, meaning that crawls would commence
      from the beginning.
    * Bumped npm version.
    
    Impact: normal

--- test/reliability.js
+++ test/reliability.js
@@ -23,4 +23,62 @@ describe("Crawler reliability",function() {
 			done();
 		});
 	});
+
+	it("should be able to freeze and defrost the queue", function(done) {
+
+		var localCrawler = new Crawler("127.0.0.1", "/", 3000),
+			newCrawler = new Crawler("127.0.0.1", "/", 3000),
+			tmp = process.env.TMPDIR + "/queue.json";
+			localCrawler.start();
+
+		var test = function() {
+			this.stop();
+
+			// Lets the queue be populated
+			process.nextTick(function() {
+				localCrawler.queue.length.should.equal(3);
+				localCrawler.queue.oldestUnfetchedIndex.should.equal(1);
+				localCrawler.queue.scanIndex["http://127.0.0.1:3000/"]
+					.should.equal(true);
+				localCrawler.queue.scanIndex["http://127.0.0.1:3000/stage2"]
+					.should.equal(true);
+				localCrawler.queue.scanIndex["http://127.0.0.1:3000/stage/3"]
+					.should.equal(true);
+
+				localCrawler.queue[0].status.should.equal("downloaded");
+				localCrawler.queue[1].status.should.equal("downloaded");
+				localCrawler.queue[2].status.should.equal("queued");
+
+				localCrawler.queue.freeze(tmp, defrost);
+			});
+		};
+
+		var defrost = function() {
+			newCrawler.queue.defrost(tmp, checkDefrost);
+		};
+
+		var checkDefrost = function() {
+			newCrawler.queue.length.should.equal(3);
+			newCrawler.queue.oldestUnfetchedIndex.should.equal(2);
+			newCrawler.queue.scanIndex["http://127.0.0.1:3000/"]
+				.should.equal(true);
+			newCrawler.queue.scanIndex["http://127.0.0.1:3000/stage2"]
+				.should.equal(true);
+			newCrawler.queue.scanIndex["http://127.0.0.1:3000/stage/3"]
+				.should.equal(true);
+
+			newCrawler.queue[0].status.should.equal("downloaded");
+			newCrawler.queue[1].status.should.equal("downloaded");
+			newCrawler.queue[2].status.should.equal("queued");
+
+			newCrawler.queue.oldestUnfetchedItem(function(err, queueItem) {
+				queueItem.url.should.equal("http://127.0.0.1:3000/stage/3");
+				done();
+			});
+		};
+
+		localCrawler.once("fetchcomplete",
+			localCrawler.once.bind(localCrawler, "fetchcomplete", test));
+
+	});
 });
