8675115ced18202f332dcd169a4afe8ba1cbadd2
Fredrik Ekelund <fredrik@fredrik.computer>
2016-07-13 23:49:59 +0200

    Only update queue items asynchronously
    Fixes #257
    
    The final puzzle piece that enables asynchronous queue
    implementations! It adds an `update` method to the queue that
    takes a URL, an object that describes the updates to be made to
    the queue item, and a callback.
    
    I've also made sure that all the places where the crawler mutates
    queueItems that are *in the queue* now use this method.
    
    And of course, I've added some tests.

--- test/queue.js
+++ test/queue.js
@@ -1,5 +1,4 @@
 /* eslint-env mocha */
-/* eslint eqeqeq: [1] */
 
 var chai = require("chai"),
     Crawler = require("../"),
@@ -7,12 +6,34 @@ var chai = require("chai"),
 
 var should = chai.should();
 
+function find(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            return array[i];
+        }
+    }
+}
+
+function deepAssign(object, source) {
+    for (var key in source) {
+        if (source.hasOwnProperty(key)) {
+            if (typeof object[key] === "object" && typeof source[key] === "object") {
+                deepAssign(object[key], source[key]);
+            } else {
+                object[key] = source[key];
+            }
+        }
+    }
+
+    return object;
+}
+
 describe("Queue methods", function() {
     var crawler = new Crawler("http://127.0.0.1:3000/");
 
     var addToQueue = function(done) {
         Object.keys(queue).forEach(function(key) {
-            if (Number(key) == key) {
+            if (!isNaN(parseInt(key, 10))) {
                 crawler.queueURL(queue[key].url);
             }
         });
@@ -187,5 +208,87 @@ describe("Queue methods", function() {
             should.not.exist(newQueueItem);
             checkDone();
         });
+    });
+
+    it("should update items in the queue", function(done) {
+        crawler.queue.update("http://127.0.0.1:3000/stage2", {
+            status: "queued",
+            fetched: false
+        }, function(error, queueItem) {
+            queueItem.should.include({
+                url: "http://127.0.0.1:3000/stage2",
+                status: "queued",
+                fetched: false
+            });
+
+            done(error);
+        });
+    });
+
+    /**
+     * This test works by monkey patching the queue `add` and `update` methods
+     * and keeping a local copy of the queue, which contains cloned queueItems.
+     * Each time the `update` method is called, we deeply compare the copy in
+     * the queue with our local one. Same thing when the crawler completes.
+     */
+    it("should only update queue items asynchronously", function(done) {
+        var crawler = new Crawler("http://127.0.0.1:3000"),
+            originalQueueAdd = crawler.queue.add,
+            originalQueueUpdate = crawler.queue.update;
+
+        var queueItems = [];
+
+        crawler.interval = 5;
+        crawler.maxDepth = 2;
+
+        function findByUrl(array, url) {
+            return find(array, function(queueItem) {
+                return queueItem.url === url;
+            });
+        }
+
+        crawler.queue.add = function (queueItem) {
+            var storedQueueItem = Object.assign({}, queueItem, {
+                status: "queued"
+            });
+            queueItems.push(storedQueueItem);
+
+            originalQueueAdd.apply(crawler.queue, arguments);
+        };
+
+        crawler.queue.update = function(url, updates) {
+            var storedQueueItem = findByUrl(queueItems, url),
+                queueQueueItem = findByUrl(crawler.queue, url);
+
+            queueQueueItem.should.eql(storedQueueItem);
+            deepAssign(storedQueueItem, updates);
+
+            originalQueueUpdate.apply(crawler.queue, arguments);
+        };
+
+        crawler.on("complete", function() {
+            crawler.queue.getLength(function(error, length) {
+                // Recursively step through items in the real queue and compare
+                // them to our local clones
+                function getItem(index) {
+                    crawler.queue.get(index, function(error, queueQueueItem) {
+                        var storedQueueItem = findByUrl(queueItems, queueQueueItem.url),
+                            nextIndex = index + 1;
+
+                        queueQueueItem.should.eql(storedQueueItem);
+
+                        if (nextIndex < length) {
+                            getItem(nextIndex);
+                        } else {
+                            done();
+                        }
+                    });
+                }
+
+                getItem(0);
+            });
+        });
+
+        crawler.start();
     });
 });
